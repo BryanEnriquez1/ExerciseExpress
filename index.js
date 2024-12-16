424878fc15982cbb18dbf197b36ba69e0e036e57
BryanEnriquez1 <bryan.enriquez487@ist17dejulio.edu.ec>
2024-12-16 10:53:27 -0500

    Practica con parametros

--- index.js
+++ index.js
@@ -13,6 +13,25 @@ var lista = [{
 app.get('/nombre',(req,res)=>{
     res.json(lista);
 });
+app.get('/buscausu/:id',(req,res)=>{
+    let id = req.params.id;
+    let usuario = lista.find(item => item.id === id);
+    res.json(usuario);
+});
+app.get('/Alexander',(req,res)=>{
+    res.send('Hola me llamo Alexander Enriquez, tengo 22 años, me gusta progrmar, entrenar MMA y tambien tocar la guitarra');
+});
+app.get('/suma',(req,res)=>{
+    let n1 = 2;
+    let n2 = 4;
+    res.send(''+(n1+n2));
+});
+app.get('/sumacli/:n1',(req,res)=>{
+    let a = parseInt(req.params.n1);
+    let b = 5;
+    let sum = a+b;
+    res.send(''+sum);
+});
 app.listen(3000,()=>{
     console.log('Framework ejecutandose con éxito');
 });
